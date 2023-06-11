280c5c17d8b955902f22059dd29138cbd0e6d9ea
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-06-11 20:49:57 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -16,6 +16,7 @@
 //
 
 // sets up dependencies
+const AWS = require('aws-sdk');
 const Alexa = require('ask-sdk-core');
 const i18n = require('i18next');
 const sprintf = require('i18next-sprintf-postprocessor');
